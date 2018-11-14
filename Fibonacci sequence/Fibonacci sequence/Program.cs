4d94b6abe0b7671ef54f4edd4a0d09be70d47520
Saeed <saeed.asemi1@gmail.com>
2018-11-14 15:10:46 +0330

    Add files via upload

---
+++ Fibonacci sequence/Fibonacci sequence/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_sequence
+{
+    class Program
+    {
+        static void Fibonacci(int n)
+        {
+            UInt64 f1, f2, t;
+            f1 = 1;
+            f2 = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                t = f1;
+                f1 = f2;
+                f2 = f2 + t;
+                Console.WriteLine(f2);
+            }
+        }
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.Write("Enter Number For Fibonacci :");
+            n = int.Parse(Console.ReadLine());
+            Fibonacci(n);
+            Console.ReadKey();
+        }
+    }
+}
